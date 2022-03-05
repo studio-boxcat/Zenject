c02ba4cc69b9cb82b8a622dc97a9d7a4d4aa0d1f
James Kim <eodnjs2998@gmail.com>
2022-03-05 15:09:08 +0900

    사용하지 않는 코드 추가 제거.

--- Source/Factories/Pooling/IMemoryPool.cs
+++ Source/Factories/Pooling/IMemoryPool.cs
@@ -47,44 +47,4 @@ namespace Zenject
     {
         TValue Spawn();
     }
-
-    public interface IMemoryPool<in TParam1, TValue> : IDespawnableMemoryPool<TValue>
-    {
-        TValue Spawn(TParam1 param);
-    }
-
-    public interface IMemoryPool<in TParam1, in TParam2, TValue> : IDespawnableMemoryPool<TValue>
-    {
-        TValue Spawn(TParam1 param1, TParam2 param2);
-    }
-
-    public interface IMemoryPool<in TParam1, in TParam2, in TParam3, TValue> : IDespawnableMemoryPool<TValue>
-    {
-        TValue Spawn(TParam1 param1, TParam2 param2, TParam3 param3);
-    }
-
-    public interface IMemoryPool<in TParam1, in TParam2, in TParam3, in TParam4, TValue> : IDespawnableMemoryPool<TValue>
-    {
-        TValue Spawn(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4);
-    }
-
-    public interface IMemoryPool<in TParam1, in TParam2, in TParam3, in TParam4, in TParam5, TValue> : IDespawnableMemoryPool<TValue>
-    {
-        TValue Spawn(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5);
-    }
-
-    public interface IMemoryPool<in TParam1, in TParam2, in TParam3, in TParam4, in TParam5, in TParam6, TValue> : IDespawnableMemoryPool<TValue>
-    {
-        TValue Spawn(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6);
-    }
-
-    public interface IMemoryPool<in TParam1, in TParam2, in TParam3, in TParam4, in TParam5, in TParam6, in TParam7, TValue> : IDespawnableMemoryPool<TValue>
-    {
-        TValue Spawn(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6, TParam7 param7);
-    }
-
-    public interface IMemoryPool<in TParam1, in TParam2, in TParam3, in TParam4, in TParam5, in TParam6, in TParam7, in TParam8, TValue> : IDespawnableMemoryPool<TValue>
-    {
-        TValue Spawn(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6, TParam7 param7, TParam8 param8);
-    }
 }
