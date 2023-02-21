1f561eec24837a77eaa202b8ac2ea3d64833ca42
James Kim <eodnjs2998@gmail.com>
2023-02-21 19:40:51 +0900

    wip

--- Source/CodeGen/DependencyProvider.cs
+++ Source/CodeGen/DependencyProvider.cs
@@ -5,25 +5,64 @@ namespace Zenject
     public readonly struct DependencyProvider
     {
         readonly DiContainer _diContainer;
-        readonly ArgumentArray _arguments;
+        readonly ArgumentArray _extraArgs;
 
 
-        public DependencyProvider(DiContainer diContainer, ArgumentArray arguments)
+        public DependencyProvider(DiContainer diContainer, ArgumentArray extraArgs)
         {
             _diContainer = diContainer;
-            _arguments = arguments;
+            _extraArgs = extraArgs;
         }
 
         public object Resolve(Type type, int identifier = default, InjectSources sourceType = default)
         {
-            if (identifier == default && _arguments.TryGetValueWithType(type, out var inst))
+            if (identifier == default && _extraArgs.TryGetValueWithType(type, out var inst))
                 return inst;
             return _diContainer.Resolve(type, identifier, sourceType);
         }
 
         public object TryResolve(Type type, int identifier = default, InjectSources sourceType = default)
         {
-            if (identifier == default && _arguments.TryGetValueWithType(type, out var inst))
+            if (identifier == default && _extraArgs.TryGetValueWithType(type, out var inst))
+                return inst;
+            return _diContainer.TryResolve(type, identifier, sourceType, out inst) ? inst : null;
+        }
+    }
+
+    public class DependencyProviderRef
+    {
+        DiContainer _diContainer;
+        ArgumentArray _extraArgs;
+
+
+        public DependencyProviderRef(DiContainer diContainer, ArgumentArray extraArgs)
+        {
+            _diContainer = diContainer;
+            _extraArgs = extraArgs;
+        }
+
+        public void Reset(DiContainer diContainer, ArgumentArray extraArgs)
+        {
+            _diContainer = diContainer;
+            _extraArgs = extraArgs;
+        }
+
+        public void Reset()
+        {
+            _diContainer = default;
+            _extraArgs = default;
+        }
+
+        public object Resolve(Type type, int identifier = default, InjectSources sourceType = default)
+        {
+            if (identifier == default && _extraArgs.TryGetValueWithType(type, out var inst))
+                return inst;
+            return _diContainer.Resolve(type, identifier, sourceType);
+        }
+
+        public object TryResolve(Type type, int identifier = default, InjectSources sourceType = default)
+        {
+            if (identifier == default && _extraArgs.TryGetValueWithType(type, out var inst))
                 return inst;
             return _diContainer.TryResolve(type, identifier, sourceType, out inst) ? inst : null;
         }
--- Source/Util/Constructor.cs
+++ Source/Util/Constructor.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
+using JetBrains.Annotations;
 using UnityEngine.Assertions;
 using Zenject.Internal;
 
@@ -9,10 +11,24 @@ namespace Zenject
     {
         public static object Instantiate(Type concreteType, DiContainer container, ArgumentArray extraArgs)
         {
+            // If the given type has generated constructor, use it.
+            var constructor = GetGeneratedConstructor(concreteType);
+            if (constructor != null)
+            {
+                var @params = RentGeneratedConstructorParams(container, extraArgs);
+                var inst = constructor.Invoke(parameters: @params);
+                ReturnGeneratedConstructorParams(@params);
+                return inst;
+            }
+
+
+            // If the constructor has no parameters, just invoke it.
             var constructorInfo = GetConstructorInfo(concreteType);
             if (constructorInfo.Parameters.Length == 0)
                 return constructorInfo.ConstructorInfo.Invoke(null);
 
+
+            // Otherwise, resolve the parameters and invoke the constructor.
 #if DEBUG
             try
 #endif
@@ -31,6 +47,22 @@ namespace Zenject
 #endif
         }
 
+        static readonly Dictionary<Type, ConstructorInfo> _generatedConstructorCache = new();
+        static readonly Type[] _generatedConstructorParamTypes = {typeof(DependencyProviderRef)};
+
+        [CanBeNull]
+        static ConstructorInfo GetGeneratedConstructor(Type type)
+        {
+            if (_generatedConstructorCache.TryGetValue(type, out var constructor))
+                return constructor;
+
+            constructor = type.GetConstructor(
+                BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance,
+                null, _generatedConstructorParamTypes, null);
+            _generatedConstructorCache.Add(type, constructor);
+            return constructor;
+        }
+
         static readonly Dictionary<Type, InjectConstructorInfo> _constructorCache = new();
 
         static InjectConstructorInfo GetConstructorInfo(Type type)
@@ -43,6 +75,26 @@ namespace Zenject
             constructorInfo = TypeAnalyzer.GetConstructorInfo(type);
             _constructorCache.Add(type, constructorInfo);
             return constructorInfo;
+        }
+
+        static readonly Stack<object[]> _generatedConstructorParamsPool = new();
+
+        static object[] RentGeneratedConstructorParams(DiContainer container, ArgumentArray extraArgs)
+        {
+            if (_generatedConstructorParamsPool.Count == 0)
+                return new object[] {new DependencyProviderRef(container, extraArgs)};
+
+            var paramValues = _generatedConstructorParamsPool.Pop();
+            var dp = (DependencyProviderRef) paramValues[0];
+            dp.Reset(container, extraArgs);
+            return paramValues;
+        }
+
+        static void ReturnGeneratedConstructorParams(object[] paramValues)
+        {
+            var dp = (DependencyProviderRef) paramValues[0];
+            dp.Reset();
+            _generatedConstructorParamsPool.Push(paramValues);
         }
     }
 }
