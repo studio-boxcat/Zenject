3dd3c02b3d787e87ae4451e7e829beaf9789ab81
James Kim <eodnjs2998@gmail.com>
2024-03-03 10:23:14 +0900

    removed ForceDispose()

--- Runtime/Contexts/Kernel.cs
+++ Runtime/Contexts/Kernel.cs
@@ -36,12 +36,5 @@ namespace Zenject
             foreach (var tickable in _lateTickables)
                 tickable.LateTick();
         }
-
-        public void ForceDispose()
-        {
-            foreach (var disposable in _disposables)
-                disposable.Dispose();
-            _disposables.Clear();
-        }
     }
 }
