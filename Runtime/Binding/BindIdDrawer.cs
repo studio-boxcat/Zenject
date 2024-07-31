2a5a8f8cd0da1af052875ed8c36b46986081bca0
James Kim <eodnjs2998@gmail.com>
2024-07-31 18:04:04 +0900

    wip

--- Runtime/Binding/BindIdDrawer.cs
+++ Runtime/Binding/BindIdDrawer.cs
@@ -1,8 +1,10 @@
 #if UNITY_EDITOR
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using JetBrains.Annotations;
 using Sirenix.OdinInspector.Editor;
-using Sirenix.Utilities.Editor;
+using UnityEditor;
 using UnityEngine;
 
 namespace Zenject
@@ -10,26 +12,27 @@ namespace Zenject
     [UsedImplicitly]
     class BindIdDrawer : OdinValueDrawer<BindId>
     {
-        static BindId[] _ids;
-        static string[] _names;
+        static int[] _ids;
+        static GUIContent[] _names;
 
         protected override void DrawPropertyLayout(GUIContent label)
         {
             if (_ids is null)
             {
-                var ids = new List<BindId> { default };
-                ids.AddRange(BindIdDict.Keys);
+                var ids = new List<int> { default };
+                ids.AddRange(BindIdDict.Keys.Select(x => (int) x));
                 _ids = ids.ToArray();
 
-                var names = new List<string> { "None" };
-                names.AddRange(BindIdDict.Values);
+                var names = new List<GUIContent> { new("None") };
+                names.AddRange(BindIdDict.Values.Select(x => new GUIContent(x)));
                 _names = names.ToArray();
             }
 
-            var bindId = ValueEntry.SmartValue;
-            if (BindIdDict.Valid(bindId) is false)
-                SirenixEditorGUI.ErrorMessageBox("BindId is not valid: " + bindId);
-            ValueEntry.SmartValue = SirenixEditorFields.Dropdown(label, bindId, _ids, _names);
+            EditorGUI.BeginChangeCheck();
+            var value = (int) ValueEntry.SmartValue;
+            var newValue = EditorGUILayout.IntPopup(label, value, _names, _ids);
+            if (EditorGUI.EndChangeCheck())
+                ValueEntry.SmartValue = (BindId) newValue;
         }
     }
 }
