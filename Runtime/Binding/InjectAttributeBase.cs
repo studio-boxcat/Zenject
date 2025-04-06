e12bedeb0da882e4385ed68f9489b4ed27b07a4f
James Kim <eodnjs2998@gmail.com>
2025-04-06 19:27:32 +0900

    fix wrong PreserveAttribute usage.

--- Runtime/Binding/InjectAttributeBase.cs
+++ Runtime/Binding/InjectAttributeBase.cs
@@ -1,10 +1,8 @@
-using System;
 using UnityEngine.Scripting;
 
 namespace Zenject
 {
-    [Preserve]
-    public abstract class InjectAttributeBase : Attribute
+    public abstract class InjectAttributeBase : PreserveAttribute
     {
         public readonly BindId Id;
         public readonly bool Optional;
